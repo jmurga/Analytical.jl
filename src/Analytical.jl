8fb5076d1ef9bc3d2bfa32807fc5018683591a5a
Jesús Murga Moreno <jesus.murga@uab.cat>
2020-03-25 17:36:02 +0100

    Solving selected polymorphism. Changes creating most of arrays

--- src/Analytical.jl
+++ src/Analytical.jl
@@ -1,46 +1,47 @@
 module Analytical
 
+using Parameters
 using PyCall
 using SpecialFunctions
 using Roots
 
-mutable struct parameters
-	gam_neg::Int64
-	gL::Int64
-	gH::Int64
-	alLow::Float64
-	alTot::Float64
-	theta_f::Float64
-	theta_mid_neutral::Float64
-	al::Float64
-	be::Float64
-	B::Float64
-	pposL::Float64
-	pposH::Float64
-	N::Int64
-	n::Int64
-	Lf::Int64
-	rho::Float64
-	L_mid::Int64
-	al2::Float64
-	be2::Float64
-	gF::Int64
-	ABC::Bool
+@with_kw mutable struct parameters
+	gam_neg::Int64 = -83
+	gL::Int64 = 10
+	gH::Int64 = 500
+	alLow::Float64 = 0.2
+	alTot::Float64 = 0.2
+	theta_f::Float64 = 1e-3
+	theta_mid_neutral::Float64 = 1e-3
+	al::Float64 = 0.184
+	be::Float64 = 0.000402
+	B::Float64 = 0.999
+	pposL::Float64 = 0.001
+	pposH::Float64 = 0
+	N::Int64 = 500
+	n::Int64 = 250
+	Lf::Int64 = 10^6
+	L_mid::Int64 = 501
+	rho::Float64 = 0.001
+	al2::Float64 =  0.0415
+	be2::Float64 = 0.00515625
+	ABC::Bool = false
 end
-mutable struct popSizeParameters
-	NN::Int64
-	nn::Int64
-end
-adap = parameters(-83,10,500,0.2,0.2,0.001,0.001,0.184,0.000402,0.999,0.001,0.0,500,250,10^6,0.001,501,0.00415,0.00515625,500,false)
-popSize = popSizeParameters(adap.N*2,adap.n*2)
 
+@with_kw mutable struct popSizeParameters
+	NN::Int64 = 1000
+	nn::Int64 = 500
+end
+
+adap = parameters()
+popSize = popSizeParameters(adap.N*2,adap.n*2)
 export adap
 
 ################################
 ###### Solving parameters ######
 ################################
 
-function Br(Lmax,theta)
+function Br(Lmax::Int64,theta::Float64)
 
 	NN = popSize.NN
 	gam_neg = adap.gam_neg
@@ -76,7 +77,6 @@ function alphaExpSimLow(pposL,pposH)
 end
 
 function alphaExpSimTot(pposL,pposH)
-
 	return (fixPosSim(adap.gL,0.5*pposL)+fixPosSim(adap.gH,0.5*pposH))/(fixPosSim(adap.gL,0.5*pposL)+fixPosSim(adap.gH,0.5*pposH)+fixNegB(0.5*pposL+0.5*pposH))
 end
 
@@ -89,6 +89,7 @@ function setPpos()
 
 	sc = pyimport("scipy.optimize")
 	pposL,pposH =  sc.fsolve(solvEqns,(0.001,0.001))
+
 	#pposL = ppos[1]; pposH = ppos[2]
 	#pposL = find_zero(alphaExpSimTot(adap.pposL,adap.pposH)-adap.alTot,0.001)
 	#pposH =  find_zero(alphaExpSimLow(adap.pposL,adap.pposH)-adap.alLow,0.001)
@@ -102,11 +103,26 @@ function setPpos()
 	adap.pposL, adap.pposH = pposL, pposH
 end
 
+function binomOp()
+
+	sc = pyimport("scipy.stats")
+	NN2 = convert(Int64, round(popSize.NN*adap.B, digits=0))
+
+	samples =  [i for i in 0:popSize.nn, j in 0:NN2]
+	samplesFreqs = [j for i in 0:popSize.nn, j in 0:NN2]
+	samplesFreqs = samplesFreqs./NN2
+
+	return sc.binom.pmf(samples,popSize.nn,samplesFreqs)
+end
+
+bn = binomOp()
+
+
 ################################
 ######     Fixations      ######
 ################################
 
-function pFix(gamma)
+function pFix(gamma::Int64)
 	s = gamma/(popSize.NN+0.0)
 	pfix = (1.0-exp(-2.0*s))/(1.0-exp(-2.0*gamma))
 	if s >= 0.1
@@ -129,7 +145,7 @@ function fixNegB(ppos::Float64)
 	return 0.745*(1-ppos)*(2^(-adap.al))*(adap.B^(-adap.al))*(adap.be^adap.al)*(-SpecialFunctions.zeta(adap.al,1.0+adap.be/(2.0*adap.B))+SpecialFunctions.zeta(adap.al,0.5*(2-1.0/(adap.N*adap.B)+adap.be/adap.B)))
 end
 
-function fixPosSim(gamma,ppos)
+function fixPosSim(gamma::Int64,ppos::Float64)
 
 	S = abs(adap.gam_neg/(1.0*popSize.NN))
 	r = adap.rho/(2.0*popSize.NN)
@@ -143,14 +159,59 @@ function fixPosSim(gamma,ppos)
 	return 0.745*ppos*exp(-2.0*S*u*(p0-p1)*CC^2/r^2)*pFix(gamma)
 end
 
-
 ################################
 ######    Polymorphism    ######
 ################################
 
-function DiscSFSSelNegDown(ppos)
+############Nuetral#############
 
-	result = adap.B*(adap.theta_mid_neutral)*0.745*(binomOp()'DiscSFSSelNeg(ppos))
+function DiscSFSNeutDown()
+	NN2 = convert(Int64,round(popSize.NN*adap.B))
+	function neutralSfs(i)
+		if i > 0 && i < NN2
+			 return 1.0/(i)
+		end
+		return 0.0
+	end
+	x = [convert(Float64,i) for i in 0:NN2]
+	solvedNeutralSfs = x .|> neutralSfs
+	return adap.B*(adap.theta_mid_neutral)*0.255*((bn*solvedNeutralSfs))
+end
+
+############Positive############
+# Variable gamma in function changed to gammaValue to avoid problem with exported SpecialFunctions.gamma
+
+function DiscSFSSelPosDown(gammaValue, ppos)
+
+	S = abs(adap.gam_neg/(1.0*popSize.NN))
+	r = adap.rho/(2.0*popSize.NN)
+	u = adap.theta_f/(2.0*popSize.NN)
+	s = gammaValue/(popSize.NN*1.0)
+	p0 = SpecialFunctions.polygamma(1,(s+S)/r)
+	p1 = SpecialFunctions.polygamma(1,1.0+(r*adap.Lf+s+S)/r)
+	red_plus = exp(-2.0*S*u*(p0-p1)/(r^2))
+
+	# Solving sfs
+	NN2 = convert(Int64,round(popSize.NN*adap.B,digits=0))
+	x = [i for i in 0:NN2]
+	x = x/(NN2+0.0)
+
+	function positiveSfs(i,gammaCorrected=gammaValue*adap.B,ppos=ppos)
+		if i > 0 && i < 1.0
+			return ppos*0.5*(exp(2*gammaCorrected)*(1-exp(-2.0*gammaCorrected*(1.0-i)))/((exp(2*gammaCorrected)-1.0)*i*(1.0-i)))
+		end
+		return 0.0
+	end
+
+	solvedPositiveSfs = (1.0/(NN2+0.0)) * (x .|> positiveSfs)
+
+	return (adap.theta_mid_neutral)*red_plus*0.745*(bn*solvedPositiveSfs
+	)
+end
+
+######Slightly deleterious######
+function DiscSFSSelNegDown(ppos)
+	result = adap.B*(adap.theta_mid_neutral)*0.745*(bn*DiscSFSSelNeg(ppos))
 	return result[1:lastindex(result)-1]
 end
 
@@ -164,24 +225,12 @@ function DiscSFSSelNeg(ppos)
 	solveZ = xa .|> z
 	if (solveZ[1] == Inf || isnan(solveZ[1]))
 		solveZ[1] = 0.0
-	elseif (solveZ[lastindex(solveZ)] == Inf || isnan([lastindex(solveZ)]))
+	end
+	if (solveZ[lastindex(solveZ)] == Inf || isnan(solveZ[lastindex(solveZ)]))
 		solveZ[lastindex(solveZ)] = 0.0
 	end
 
 	return 1.0/(popSize.NN+0.0).*solveZ
-end
-
-function binomOp()
-
-	sc = pyimport("scipy.stats")
-	NN2 = convert(Int64, round(popSize.NN*adap.B, digits=0))
-
-	# samples = permutedims([i for j in 0:NN2, i in 1:popSize.NN])
-	samples =  permutedims([i for j in 0:NN2, i in 0:popSize.nn])
-	samplesFreqs = permutedims([round(j/(NN2+0.0),digits=8) for j in 0:NN2, i in 0:popSize.nn])
-
-	#return samples,popSize.NN,samplesFreqs
-	return sc.binom.pmf(samples,popSize.nn,samplesFreqs)
 end
 
 function cumulativeSfs(sfsTemp)
@@ -199,4 +248,49 @@ function cumulativeSfs(sfsTemp)
 	end
 	return out
 end
+
+################################
+###    Summary statistics    ###
+################################
+
+function alphaByFrequencies(gammaL,gammaH,pposL,pposH)
+
+	ret = Array{Float64}(undef, popSize.nn - 1)
+	sel = Array{Float64}(undef, popSize.nn - 1)
+	# Fixation
+	fN = adap.B*fixNeut()
+	fNeg = adap.B*fixNegB(0.5*pposH+0.5*pposL)
+	fPosL = fixPosSim(gammaL,0.5*pposL)
+	fPosH = fixPosSim(gammaH,0.5*pposH)
+
+	# Polymorphism
+	neut = cumulativeSfs(DiscSFSNeutDown())
+	selH = cumulativeSfs(DiscSFSSelPosDown(gammaH,pposH))
+	selL = cumulativeSfs(DiscSFSSelPosDown(gammaL,pposL))
+	selN = cumulativeSfs(DiscSFSSelNegDown(adap.pposH+adap.pposL))
+
+	# for i in range(0,len(selH)):
+	# 	sel.append((selH[i]+selL[i])+selN[i])
+	# for i in range(0,self.nn-1):
+	# 	ret.append(float(1. - (fN/(fPosL + fPosH+  fNeg+0.))* sel[i]/neut[i]))
+	return (neut,selH,selL,selN,fN,fNeg,fPosL,fPosH)
+end
+
+# set_theta_f()
+# setPpos()
+#
+# function test()
+#
+# 	for t in 1:10^6
+# 		c,d,e,f,g,h,i,j = alphaByFrequencies(adap.gL,adap.gH,adap.pposH,adap.pposL)
+#
+# end
+################################
+#### Old functions equations ###
+################################
+#
+# alphaExpSimLow(pposL,pposH) = fixPosSim(adap.gL,0.5*pposL)/(fixPosSim(adap.gL,0.5*pposL)+fixPosSim(adap.gH,0.5*pposH) + fixNegB(0.5*pposL+0.5*pposH))
+#
+# alphaExpSimTot(pposL,pposH) = (fixPosSim(adap.gL,0.5*pposL)+fixPosSim(adap.gH,0.5*pposH))/(fixPosSim(adap.gL,0.5*pposL)+fixPosSim(adap.gH,0.5*pposH)+fixNegB(0.5*pposL+0.5*pposH))
+
 end # module
