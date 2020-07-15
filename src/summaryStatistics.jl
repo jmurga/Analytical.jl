a8b81c9345148c6ef08a893c289c65fb0b90030c
Jesús Murga Moreno <jesus.murga@uab.cat>
2020-07-15 16:56:44 +0200

    alphas from estimations

--- src/summaryStatistics.jl
+++ src/summaryStatistics.jl
@@ -106,33 +106,6 @@ function sampledAlpha(;d::Array,afs::Array,λdiv::Array{Float64,2},λpol::Array{
 
 end
 
-function resampleByIndex(;param::parameters,bArr::BitArray{1},alpha::Array{Float64,1},afs::Array{Int64,2},pn::Array{Float64,1},ps::Array{Float64,1},Pn::Array{Int64,2},Ps::Array{Int64,2},Dn::Array{Int64,1},Ds::Array{Int64,1},stats::Array{Float64,2},bins::Int64,cutoff::Float64)
-	
-	while sum(bArr) < size(alpha,1)
-		id = findall(x ->x == false, bArr)
-	
-		if size(id,1) < 2
-			id = id[1]
-		end
-	
-		tmpPn, tmpPs    = poissonPolymorphism(observedValues=afs[:,id],λps=ps,λpn=pn)
-
-		Pn[:,id] = tmpPn; Ps[:,id] = tmpPs;
-		reduceExpPn = view(reduceSfs(tmpPn,bins)',1:bins,:);
-		reduceExpPs = view(reduceSfs(tmpPs,bins)',1:bins,:);
-	
-		stats[:,id] = @. 1 - ((Ds[id]/Dn[id])' * (reduceExpPn./reduceExpPs));
-		stats = round.(stats,digits=5);
-
-		tmpAlpha = @. 1 - ((Ds[id]/Dn[id])' * (tmpPn/tmpPs))
-		alpha[id,:] =  @view tmpAlpha[trunc(Int64,param.nn*cutoff),:]
-		bArr[id,:] = alpha[id,:] .> 0 
-		
-	end
-
-	return alpha,stats
-end
-
 """
 	alphaByFrequencies(gammaL,gammaH,pposL,param.pposH,data,nopos)
 
@@ -295,110 +268,11 @@ function alphaByFrequencies(param::parameters,divergence::Array,sfs::Array,bins:
 	Dn,Ds,Pn,Ps = expectedDn,expectedDs,sum(view(expectedPn,1,:),dims=2),sum(view(expectedPs,1,:),dims=2)
 	
 	# alphas = round.(hcat(param.alTot - alLow, param.alLow, param.alTot),digits=5)
-	# alphas = round.(hcat(αW_nopos[trunc(Int64,param.nn*cutoff),:], αS_nopos[trunc(Int64,param.nn*cutoff),:], α_nopos[trunc(Int64,param.nn*cutoff),:]),digits=5)	
-	alphas = round.(hcat(param.alLow, param.alTot-param.alLow,param.alTot),digits=5)	
+	alphas = round.(hcat(αW_nopos[trunc(Int64,param.nn*cutoff),:], αS_nopos[trunc(Int64,param.nn*cutoff),:], α_nopos[trunc(Int64,param.nn*cutoff),:]),digits=5)	
+	# alphas = round.(hcat(param.alLow, param.alTot-param.alLow,param.alTot),digits=5)	
 	alphas = repeat(alphas,outer=[size(divergence,1),1])	
 
 	expectedValues = hcat(DataFrame(alphas),DataFrame(hcat(Dn,Ds,Pn,Ps)),DataFrame(permutedims(alxSummStat)),makeunique=true)
-
-	return (α,α_nopos,expectedValues)
-end
-
-function alphaByFrequenciesSampled(param::parameters,divergence::Array,sfs::Array{Int64,2},bins::Int64,cutoff::Float64)
-
-	##############################################################
-	# Accounting for positive alleles segregating due to linkage #
-	##############################################################
-
-	# Fixation
-	fN     = param.B*fixNeut(param)
-	fNeg   = param.B*fixNegB(param,0.5*param.pposH+0.5*param.pposL)
-	fPosL  = fixPosSim(param,param.gL,0.5*param.pposL)
-	fPosH  = fixPosSim(param,param.gH,0.5*param.pposH)
-
-	ds = fN
-	dn = fNeg + fPosL + fPosH
-
-	## Polymorphism
-	neut = DiscSFSNeutDown(param)
-
-	selH = DiscSFSSelPosDown(param,param.gH,param.pposH);
-	selL = DiscSFSSelPosDown(param,param.gL,param.pposL);
-	selN = DiscSFSSelNegDown(param,param.pposH+param.pposL);
-
-	sel = (selH+selL)+selN;
-
-	cumulativePs = cumulativeSfs(neut)[:,1]
-	cumulativePn = cumulativeSfs(sel)[:,1]
-
-	## Outputs
-	# α = sampledAlpha(d=divergence,afs=sfs,λdiv=hcat(ds,dn),λpol=hcat(neut,sel),expV=false,bins=bins)
-	# α = view(α,1:trunc(Int64,param.nn*cutoff),:)
-
-	# α, expectedDn, expectedDs, expectedPn, expectedPs, summStat = sampledAlpha(d=divergence,afs=sfs,λdiv=hcat(ds,dn),λpol=hcat(cumulativePs,cumulativePn),expV=true,bins=bins)
-	expDn, expDs    = poissonFixation(observedValues=divergence,λds=ds,λdn=dn);
-	expPn, expPs    = poissonPolymorphism(observedValues=sfs,λps=cumulativePs,λpn=cumulativePn);
-	reduceExpPn = view(permutedims(reduceSfs(expPn,bins)),1:bins,:);
-	reduceExpPs = view(permutedims(reduceSfs(expPs,bins)),1:bins,:);
-
-	## Alpha from expected values. Used as summary statistics
-	summStat = @. 1 - ((expDs/expDn)' * (reduceExpPn./reduceExpPs));
-	summStat = round.(summStat,digits=5)
-	
-	tmp = @. 1 - ((expDs/expDn)' * (expPn/expPs));
-	α = tmp[trunc(Int64,param.nn*cutoff),:]
-
-	boolArr = (α .> 0) .& (α .< param.alTot)
-
-	α, summStat = resampleByIndex(param=param,bArr=boolArr,alpha=α,afs=sfs,pn=cumulativePn,ps=cumulativePs,Pn=expPn,Ps=expPs,Dn=expDn,Ds=expDs,stats=summStat,bins=bins,cutoff=cutoff)
-
-	##################################################################
-	# Accounting for for neutral and deleterious alleles segregating #
-	##################################################################
-	## Fixation
-	fN_nopos     = fN*(param.theta_mid_neutral/2.)*param.TE*param.NN
-	fNeg_nopos   = fNeg*(param.theta_mid_neutral/2.)*param.TE*param.NN
-	fPosL_nopos  = fPosL*(param.theta_mid_neutral/2.)*param.TE*param.NN
-	fPosH_nopos  = fPosH*(param.theta_mid_neutral/2.)*param.TE*param.NN
-
-	ds_nopos = fN_nopos
-	dn_nopos = fNeg_nopos + fPosL_nopos + fPosH_nopos
-
-	## Polymorphism
-	sel_nopos = selN
-	cumulativePn_nopos = cumulativeSfs(sel_nopos)[:,1]
-
-	## Outputs
-	expPn_nopos, expPs_nopos    = poissonPolymorphism(observedValues=sfs,λps=cumulativePs,λpn=cumulativePn_nopos)
-	tmp_nopos = @. 1 - ((expDs/expDn)' * (expPn_nopos/expPs_nopos))
-	α_nopos = tmp_nopos[trunc(Int64,param.nn*cutoff),:]
-	
-	alBoolArr = α_nopos .> α
-	while sum(alBoolArr) < size(α_nopos,1)
-		id = findall(x ->x == false, alBoolArr)
-	
-		if size(id,1) < 2
-			id = id[1]
-		end
-	
-		tmpPn, tmpPs = poissonPolymorphism(observedValues=sfs[:,id],λps=cumulativePs,λpn=cumulativePn_nopos)
-		expPn_nopos[:,id] = tmpPn; expPs_nopos[:,id] = tmpPs;
-
-		tmpAlpha = @. 1 - ((expDs[id]/expDn[id])' * (tmpPn/tmpPs))
-		α_nopos[id,:] =  @view tmpAlpha[trunc(Int64,param.nn*cutoff),:]
-		alBoolArr[id,:] = α_nopos[id,:] .> α[id,:]
-
-	end
-
-	##########
-	# Output #
-	##########
-	Dn,Ds,Pn,Ps = expDn,expDs,sum(view(expPn,1,:),dims=2),sum(view(expPs,1,:),dims=2)
-
-	alphas = round.(hcat(α, α_nopos .- α, α_nopos),digits=5)
-	# alphas = repeat(alphas,outer=[2,1])
-
-	expectedValues = hcat(DataFrame(alphas),DataFrame(hcat(Dn,Ds,Pn,Ps)),DataFrame(permutedims(summStat)),makeunique=true)
 
 	return (α,α_nopos,expectedValues)
 end
@@ -441,35 +315,3 @@ function asympFit(alphaValues::Array{Float64,1})
 	# return [asymp ciLow ciHigh]
 	return asymp
 end
-
-# function scipyFit(alphaValues::Array{Float64,1})
-
-# 	x = collect(1:size(alphaValues,1))
-# 	py"""
-# 	import numpy as np
-# 	# from numba import *
-# 	from scipy import optimize
-# 	# @njit(cache=True)
-# 	def exp_model(x,a,b,c):
-# 		return a + b*np.exp(-x*c)
-# 	# @jit(cache=True)
-# 	def test(x1,al):
-# 		res = {}
-# 		model = optimize.curve_fit(exp_model,x1, al, method='dogbox')
-
-# 		res['a'] = model[0][0]
-# 		res['b'] = model[0][1]
-# 		res['c'] = model[0][2]
-
-# 		# alpha for predicted model
-# 		res['alpha'] = exp_model(x1[-1], res['a'], res['b'], res['c'])
-# 		return(res['alpha'])
-# 	"""
-		
-# 	# plot(x,alphaTrim)
-# 	# plot!(x,asympModel(x,fitted.param),legend=:bottomleft)
-
-# 	return py"test"(PyObject(x),PyObject(alphaValues[:,1]))
-
-# end
-
