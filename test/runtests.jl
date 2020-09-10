6b45db9fca6624df43bc9827d98cde1f8fd51528
Jesús Murga Moreno <jesus.murga@uab.cat>
2020-09-10 17:26:04 +0200

    changing julia travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Test
 # Running Analytical approximation with default parameters
 @testset "Analytical.jl" begin
 
-	adap = Analytical.parameters(N=500,n=661,alLow=0.2)
+    adap = Analytical.parameters(N=500,n=661,alLow=0.2)
     Analytical.binomOp!(adap)
-	Analytical.set_theta_f!(adap)
+    Analytical.set_theta_f!(adap)
     Analytical.setPpos!(adap)
     
     @test adap.pposH == 0.0
-	@test adap.theta_f == 5.417709306355064e-7
+    @test adap.theta_f == 5.417709306355064e-7
 
 end
