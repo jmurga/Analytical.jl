f63bc21f5b6be99af11f74e1003de5344cf76fef
jmurga <jesus.murga@uab.cat>
2020-11-18 17:25:25 +0100

    reset cumulative rates

--- src/summaryStatistics.jl
+++ src/summaryStatistics.jl
@@ -278,7 +278,6 @@ function alphaByFrequencies(param::parameters,divergence::Array,sfs::Array,bins:
 	selL = DiscSFSSelPosDown(param,param.gL,param.pposL,param.bn[param.B])
 	selN = DiscSFSSelNegDown(param,param.pposH+param.pposL,param.bn[param.B])
 	tmp = cumulativeSfs(hcat(neut,selH,selL,selN))
-	tmp = hcat(neut,selH,selL,selN)
 	splitColumns(matrix::Array{Float64,2}) = (view(matrix, :, i) for i in 1:size(matrix, 2));
 
 	neut, selH, selL, selN = splitColumns(tmp)
