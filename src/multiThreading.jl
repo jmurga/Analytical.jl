25a6ebf1475f77d7b2c45e4bc0ec70800af5be3e
jmurga <jesus.murga@uab.cat>
2020-12-21 12:05:30 +0100

    adding B

--- src/multiThreading.jl
+++ src/multiThreading.jl
@@ -21,7 +21,13 @@ function summaryStats(;param::parameters,amk::Float64,shape::Float64=0.184,scale
 	fac         = rand(-2:0.05:2,iterations,2)
 	afac        = @. shape*(2^fac[:,1]) 
 	bfac        = @. scale*(2^fac[:,2])
-	alTot       = rand(collect(alpha:0.05:(alpha*2)),iterations)
+
+	if amk > 0.7
+		alTot = rand(collect(amk:0.05:0.95,iterations))
+	else
+		alTot = rand(collect(amk:0.05:(amk+0.3)),iterations)
+	end
+
 	lfac        = rand(collect(0.1:0.1:0.9),iterations)
 	alLow       = @. round(alTot * lfac,digits=5)
 	nParam      = [param for i in 1:iterations]
@@ -32,14 +38,14 @@ function summaryStats(;param::parameters,amk::Float64,shape::Float64=0.184,scale
 	# Estimations to thread pool
 	# wp  = Distributed.CachingPool(Distributed.workers())
 	# tmp = Distributed.pmap(bgsIter,wp,nParam,afac,bfac,alTot,alLow,ndivergence,nSfs,nDac);
-	out = SharedArray{Float64,3}((iterations,19,(3+size(dac,1))))
+	out = SharedArray{Float64,3}((iterations,size(param.bRange,2),(4+size(dac,1))))
 	@sync @distributed for i in eachindex(afac)
 		tmp = bgsIter(nParam[i],afac[i],bfac[i],alTot[i],alLow[i],ndivergence[i],nSfs[i],nDac[i])
 		out[i,:,:] = tmp
 	end
 
 	# Output
-	df = reshape(out,iterations*size(param.bRange,2), (3+size(dac,1)))
+	df = reshape(out,iterations*size(param.bRange,2), (4+size(dac,1)))
 	# df  = reduce(vcat,tmp)
 	# idx = vcat(1:3,3 .+ dac)
 	# df  = df[:,idx]
@@ -62,7 +68,7 @@ function bgsIter(param::parameters,afac::Float64,bfac::Float64,alTot::Float64,al
 
 	# Matrix and values to solve
 	dm 			= size(divergence,1)
-	r           = Array{Float64}(undef, 19 * dm , size(dac,1) + 3)
+	r           = Array{Float64}(undef, size(param.bRange,2) * dm , size(dac,1) + 4)
 	param.al    = afac; param.be = bfac;
 	param.alLow = alLow; param.alTot = alTot;
 
