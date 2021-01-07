223734552f8efc51a90bdf2e6904c7ae04421e01
Jesús Murga Moreno <jmurga@pop-os.localdomain>
2021-01-07 23:15:01 +0100

    adding multiple s to abc

--- src/multiThreading.jl
+++ src/multiThreading.jl
@@ -11,8 +11,7 @@ Function to solve randomly *N* scenarios
 # Returns
  - `Array`: summary statistics
 """
-function summaryStats(;param::parameters,amk::Float64,shape::Float64=0.184,scale::Float64=0.000402,divergence::Array,sfs::Array,dac::Array{Int64,1},iterations::Int64,fixed::Bool=false)
-
+function summaryStats(;param::parameters,amk::Float64,gH::Array{Int64,1},gL::Array{Int64,1},shape::Float64=0.184,scale::Float64=0.000402,divergence::Array,sfs::Array,dac::Array{Int64,1},iterations::Int64,fixed::Bool=false)
 
 	# iterations  = trunc(Int,iterations/19) + 1
 	# N random prior combinations
@@ -32,19 +31,20 @@ function summaryStats(;param::parameters,amk::Float64,shape::Float64=0.184,scale
 		#=lfac = rand(0.1:0.05:0.9,iterations)=#
 	end
 
-	alTot = rand(0.1:0.01:0.9,iterations)
+	nTot = rand(0.1:0.01:0.9,iterations)
 
-	alLow       = @. alTot * lfac
+	nLow       = @. nTot * lfac
 	nParam      = [param for i in 1:iterations]
 	ndivergence = [divergence for i in 1:iterations]
 	nSfs        = [sfs for i in 1:iterations]
 	nDac        = [dac for i in 1:iterations]
-
+	ngh = rand(repeat(gH,iterations),iterations)
+	ngl = rand(repeat(gL,iterations),iterations)
 	# Estimations to thread pool
 
 	out = SharedArray{Float64,3}((iterations,size(param.bRange,2),(3+size(dac,1))))
 	@sync @distributed for i in eachindex(afac)
-		tmp = Analytical.bgsIter(nParam[i],afac[i],bfac[i],alTot[i],alLow[i],ndivergence[i],nSfs[i],nDac[i])
+		tmp = bgsIter(param = nParam[i],alTot = nTot[i], alLow = nLow[i],gH=ngh[i],gL=ngl[i],afac=afac[i],bfac=bfac[i],divergence=ndivergence[i],sfs=nSfs[i],dac=nDac[i])
 		out[i,:,:] = tmp
 	end
 
@@ -65,20 +65,21 @@ Function to input and solve one scenario given *N* background selection values (
 # Returns
  - `Array`: summary statistics
 """
-function bgsIter(param::parameters,afac::Float64,bfac::Float64,alTot::Float64,alLow::Float64,divergence::Array,sfs::Array,dac::Array{Int64,1})
+function bgsIter(;param::parameters,alTot::Float64,alLow::Float64,gH::Int64,gL=Int64,afac::Float64,bfac::Float64,divergence::Array,sfs::Array,dac::Array{Int64,1})
 
 	# Matrix and values to solve
 	dm 			= size(divergence,1)
 	r           = Array{Float64}(undef, size(param.bRange,2) * dm , size(dac,1) + 3)
 	param.al    = afac; param.be = bfac;
 	param.alLow = alLow; param.alTot = alTot;
-
+	param.gH = gH;param.gL = gL
 	# Solve probabilites without B effect to achieve α value
 	param.B = 0.999
 	setThetaF!(param)
 	setPpos!(param)
 
-	iter = 1
+	
+	r = zeros(dm,size(param.bRange,2) * dm , size(dac,1) + 3)
 	for j in eachindex(param.bRange)
 		param.B = param.bRange[j]
 		# Solve mutation given a new B value.
@@ -86,9 +87,9 @@ function bgsIter(param::parameters,afac::Float64,bfac::Float64,alTot::Float64,al
 		# Solven given same probabilites probabilites ≠ bgs mutation rate.
 		x,y,z::Array{Float64,2} = alphaByFrequencies(param,divergence,sfs,dac)
 		# push!(r,z)
-		r[iter:(iter + (dm - 1)),:] = z
-		iter = iter + dm
+		r[j,:,:] = z
 	end
+	r = reshape(r,dm*size(param.bRange,2),size(dac,1)+3)
 
 	return r
 end
