53c29506d7f2133af3c39e153b04f3738dd4fe10
jmurga <jesus.murga@uab.cat>
2022-01-11 17:08:14 +0100

    plot bugs

--- scripts/plot_map_r.jl
+++ scripts/plot_map_r.jl
@@ -8,7 +8,7 @@ catch
 
 	using Conda
 	Conda.add("r-base",channel="conda-forge")
-	Conda.add(["r-locfit","r-ggplot2","r-data.table","r-r.utils"],channel="conda-forge")
+	Conda.add(["r-locfit","r-ggplot2","r-r.utils"],channel="conda-forge")
 
 	Pkg.add("RCall")
 
@@ -20,45 +20,44 @@ end
 """
 function plot_map(;analysis_folder::String,weak::Bool=true,title::String="Posteriors")
 
-	try
-		R"""library(ggplot2);library(locfit);library(data.table)"""
+	R"""library(locfit);library(ggplot2);library(data.table)""";
 
-		out = filter(x -> occursin("post",x), readdir(analysis_folder,join=true))
-		out          = filter(x -> !occursin(".1.",x),out)
+	out = filter(x -> occursin("post",x), readdir(analysis_folder,join=true))
+	out          = filter(x -> !occursin(".1.",x),out)
 
-		open(x)      = Array(CSV.read(x,DataFrame,header=false))
+	open(x)      = Array(CSV.read(x,DataFrame,header=false))
 
-		# Control outlier inference. 2Nes non negative values
-		flt(x)       = x[x[:,4] .> 0,:]
-		posteriors   = flt.(open.(out))
+	# Control outlier inference. 2Nes non negative values
+	flt(x)       = x[x[:,4] .> 0,:]
+	posteriors   = flt.(open.(out))
 
-		R"""getmap <- function(df){
-				temp = as.data.frame(df)
-				d <-locfit(~temp[,1],temp);
-				map<-temp[,1][which.max(predict(d,newdata=temp))]
-			}"""
+	R"""getmap <- function(df){
+			temp = as.data.frame(df)
+			d <-locfit(~temp[,1],temp);
+			map<-temp[,1][which.max(predict(d,newdata=temp))]
+		}"""
 
-		getmap(x)    = rcopy(R"""suppressWarnings(matrix(apply($x,2,getmap),nrow=1))""")
-		
-		if !weak
-			posteriors = [posteriors[i][:,3:end] for i in eachindex(posteriors)]
-			tmp          = getmap.(posteriors)
-			maxp         = DataFrame(vcat(tmp...),[:a,:gamNeg,:shape])
-			al           = maxp[:,1:1]
-			gam          = maxp[:,2:end]
-		else
-			tmp          = getmap.(posteriors)
-			maxp         = DataFrame(vcat(tmp...),[:aw,:as,:a,:gamNeg,:shape])
-			al           = maxp[:,1:3]
-			gam          = maxp[:,4:end]
-		end
+	getmap(x)    = rcopy(R"""suppressWarnings(matrix(apply($x,2,getmap),nrow=1))""")
+	
+	if !weak
+		posteriors = [posteriors[i][:,3:end] for i in eachindex(posteriors)]
+		tmp          = getmap.(posteriors)
+		maxp         = DataFrame(vcat(tmp...),[:a,:gamNeg,:shape])
+		al           = maxp[:,1:1]
+		gam          = maxp[:,2:end]
+	else
+		tmp          = getmap.(posteriors)
+		maxp         = DataFrame(vcat(tmp...),[:aw,:as,:a,:gamNeg,:shape])
+		al           = maxp[:,1:3]
+		gam          = maxp[:,4:end]
+	end
 
-		@rput al
-		@rput posteriors
-		@rput analysis_folder
-		@rput title
+	@rput al
+	@rput title
+	@rput posteriors
+	@rput analysis_folder
 
-		R"""al = as.data.table(al)
+	R"""al = as.data.table(al)
 			lbls = if(ncol(al) > 1){c(expression(paste('Posterior ',alpha[w])), expression(paste('Posterior ',alpha[s])),expression(paste('Posterior ',alpha)))}else{c(expression(paste('Posterior ',alpha)))}
 			clrs = if(ncol(al) > 1){c('#30504f', '#e2bd9a', '#ab2710')}else{c('#ab2710')}
 
@@ -72,10 +71,8 @@ function plot_map(;analysis_folder::String,weak::Bool=true,title::String="Poster
 			}
 			suppressWarnings(ggsave(pal,filename=paste0(analysis_folder,'/map.png'),dpi=600))
 			"""
-		CSV.write(analysis_folder * "/map.tsv",maxp,delim='\t',header=true)
 
-		return(maxp)
-	catch
-		println("Please install R, ggplot2, data.table and locfit in your system before execute this function")
-	end
+	CSV.write(analysis_folder * "/map.tsv",maxp,delim='\t',header=true)
+
+	return(maxp)
 end
